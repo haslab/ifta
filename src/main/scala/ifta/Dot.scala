27b0bb88dead72bbc6ddd72348cfb57aa52a6137
Jose Proenca <jose.proenca@cs.kuleuven.be>
2016-09-30 17:36:46 +0100

    added toDot conversion (showed composition does not work yet)

---
+++ src/main/scala/ifta/Dot.scala
@@ -0,0 +1,21 @@
+package ifta
+
+/**
+  * Created by jose on 30/09/16.
+  */
+object Dot {
+  def apply(ifta: IFTA): String = {
+    val edges = toDotEdges(ifta.edges)
+    "digraph G {\n  rankdir=LR;\n  node [margin=0 width=0.2 height=0.2 label=\"\"]\n"++
+      "  edge [arrowsize=0.7]\n"++
+      s"$edges}"
+  }
+
+  def toDotEdges(edges: Iterable[Edge]) = {
+    val res = new StringBuilder
+    for (e <- edges)
+      res append s"""${e.from} -> ${e.to} [label="${e.act.mkString("/")}"]\n"""
+    res.toString()
+  }
+
+}
