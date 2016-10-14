df8457378c504ced71374162731ab3caac5c123d
Jose Proenca <jose.proenca@cs.kuleuven.be>
2016-10-14 15:17:25 +0100

    fixed example: connectors have options

--- src/main/scala/ifta/Examples.scala
+++ src/main/scala/ifta/Examples.scala
@@ -27,27 +27,36 @@ object Examples {
   ////////////////////
   // Reo connectors //
   ////////////////////
+  def v(s:String) = "v"+s
+
   def router(i:String,o1:String,o2:String) = newifta ++ (
-    0 --> 0 by s"$i,$o1" when s"v$i" && s"v$o1",
-    0 --> 0 by s"$i,$o2" when s"v$i" && s"v$o2"
-    ) get i pub s"$o1,$o2"
+    0 --> 0 by s"$i,$o1" when v(i) && v(o1),
+    0 --> 0 by s"$i,$o2" when v(i) && v(o2)
+    ) get i pub s"$o1,$o2" when (v(o1) || v(o2)) --> v(i)
 
   def fifo(i:String,o:String) = newifta ++ (
-    0 --> 1 by s"$i" when s"v$i",
-    1 --> 0 by s"$o" when s"v$o"
-    ) get i pub o
+    0 --> 1 by s"$i" when v(i) && v(o),
+    1 --> 0 by s"$o" when v(i) && v(o),
+    0 --> 0 by s"$i" when v(i) && not(v(o))  // can receive if there is no output
+    ) get i pub o   when v(o) --> v(i)
 
   def sync(i:String,o:String) = newifta ++ (
-    0 --> 0 by s"$i,$o" when s"v$i" && s"v$o"
-    ) get i pub o
+    0 --> 0 by s"$i,$o" when v(i) && v(o),
+    0 --> 0 by s"$i" when v(i) && not(v(o))  // can receive if there is no output
+//    0 --> 0 by s"$o" when not(v(i)) && v(o) // drop: cannot produce if there is no input
+    ) get i pub o  when v(o) --> v(i)
 
   def sdrain(i:String,i2:String) = newifta ++ (
-    0 --> 0 by s"$i,$i2" when s"v$i" && s"v$i2"
+    0 --> 0 by s"$i,$i2" when v(i) && v(i2),
+    0 --> 0 by s"$i" when v(i) && not(v(i2)),
+    0 --> 0 by s"$i2" when v(i2) && not(v(i))
     ) get i get i2
 
   def repl(i:String,o1:String,o2:String) = newifta ++ (
-    0 --> 0 by s"$i,$o1,$o2" when s"v$i" && s"v$o1" && s"v$o2"
-    ) get i pub o1 pub o2
+    0 --> 0 by s"$i,$o1,$o2" when v(i) && v(o1) && v(o2),
+    0 --> 0 by s"$i,$o1" when v(i) && v(o1),
+    0 --> 0 by s"$i,$o2" when v(i) && v(o2)
+    ) get i pub o1 pub o2  when (v(o1) || v(o2)) --> v(i)
 
   // examples:
   // repl("a","b","c") *  sync("b","d") *  fifo("c","e") // compose 3 connectors with product
