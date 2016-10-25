b8e224df060006c02234c08719f3005d236247d3
Guillermina Cledou <guillecledou@gmail.com>
2016-10-25 22:01:27 +0100

    Some extra connectors

--- src/main/scala/ifta/Examples.scala
+++ src/main/scala/ifta/Examples.scala
@@ -80,12 +80,16 @@ object Examples {
     0 --> 0 by s"$i" when v(i) && not(v(o1) || v(o2))     // can behave as asyncdrain if only the input is present
     ) get i pub o1 pub o2  when (v(o1) || v(o2)) --> v(i)
 
+  // other examples
+  val router3 = (router("i","o1","o2") * router("i1","o3","o4")) sync(("o1","i1"))
+  //  sequencer a>b>c :
+  val sequencer3network = fifo("i","o") || repl("o","a","o2") || fifo("o2","o3") || repl("o3","b","o5") || fifo("o5","o6") || repl("o6","c","i")
+  val sequencer3 = fifo("i","o") * repl("o","a","o2") * fifo("o2","o3") * repl("o3","b","o5") * fifo("o5","o6") * repl("o6","c","i")
+
   // examples:
   // repl("a","b","c") *  sync("b","d") *  fifo("c","e") // compose 3 connectors with product
   // repl("a","b","c") || sync("b","d") || fifo("c","e") // network of 3 connectors
   // note: last one works worse in UPPAAL: it is possible to reach deadlock by chosing the wrong sequence of committed
   //      locations (UPPAAL is not great with sequences of committed states).
 
-//  example small pipeline a>b>c:
-//  var pipe = fifo("i","o") || repl("o","a","o2") || fifo("o2","o3") || repl("o3","b","o5") || fifo("o5","o6") || repl("o6","c","i")
 }
