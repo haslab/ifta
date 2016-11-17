416ae6fef62bcb35f4f635fb089ebb1aeb9b35f8
Jose Proenca <jose.proenca@cs.kuleuven.be>
2016-11-17 14:54:10 +0000

    added families of connectors (with relax option)

--- src/main/scala/ifta/reo/FamConnectors.scala
+++ src/main/scala/ifta/reo/FamConnectors.scala
@@ -16,12 +16,78 @@ import ifta._
   */
 object FamConnectors {
 
+  /** Wrap for IFTAs extended with `relax` and `exclusive` methods. */
+  class Conn(locs:Set[Int], init:Int, act:Set[String], clocks:Set[String]
+             , feats:Set[String], edges:Set[Edge], cInv:Map[Int,ClockCons], fm:FExp
+             , in:Set[String], out:Set[String], aps:Map[Int,String], shortname:String="")
+  extends IFTA(locs,init,act,clocks,feats,edges,cInv,fm,in,out,aps,shortname) {
+    def relax: Conn =
+      new Conn(locs,init,act,clocks,act.map(v),edges.map(relaxEdge),cInv,fm,in,out,aps,shortname)
+    private def relaxEdge(e: Edge):Edge =
+      Edge(e.from,e.cCons,e.act,e.cReset,e.act.foldRight[FExp](FTrue)((s,fs)=>v(s)&&fs),e.to)
+    override def when(f: FExp): Conn =
+      new Conn(locs,init,act,clocks,feats++f.feats,edges,cInv,fm && f,in,out,aps,shortname)
+    def exclude(p:String): Conn = exclude(Set(p))
+    def exclude(ps:Set[String]): Conn =
+      new Conn(locs,init,act,clocks,act,edges.map(excludeEdge(ps,act)),cInv,fm,in,out,aps,shortname)
+    private def excludeEdge(ps:Set[String],as:Set[String])(e:Edge): Edge =
+      if (ps != e.act) e
+      else Edge(e.from,e.cCons,e.act,e.cReset,
+            e.act.foldRight[FExp](FTrue)((s,fs)=>v(s)&&fs) &&
+              (as -- e.act).foldRight[FExp](FTrue)((s,fs)=>not(v(s))&&fs),
+            e.to)
+  }
+
   // auxilary constructions
   private def v(s:String) = "v_"+s
   private def mkFeat(act:Set[String]):Set[FExp] = act.map(a => Feat(v(a)))
   private def mkFAnd(feats:Set[FExp]):FExp = Simplify(feats.fold(FTrue)(_&&_))
   private def mkFOr(feats:Set[FExp]):FExp = Simplify(feats.fold(FNot(FTrue))(_||_))
+  private def mkConn(i:IFTA) =
+    new Conn(i.locs,i.init,i.act,i.clocks,i.feats,i.edges,i.cInv,i.fm,i.in,i.out,i.aps,i.shortname)
 
+
+  def fifo(i:String,o:String) = {
+    val feat = v(s"${i}_${o}")
+    mkConn(newifta ++ (
+      0 --> 1 by s"$i" when feat,
+      1 --> 0 by s"$o" when feat,
+      0 --> 0 by s"$i" when feat
+      ) get i pub o   name "[]")
+  }
+
+  def fifofull(i:String,o:String) = {
+    val feat = v(s"${i}_${o}")
+    mkConn(newifta ++ (
+      0 --> 1 by s"$o" when feat,
+      1 --> 0 by s"$i" when feat
+      ) get i pub o  name "[.]")
+  }
+
+  def sync(i:String,o:String) = {
+    val feat = v(s"${i}_${o}")
+    mkConn(newifta ++ (
+      0 --> 0 by s"$i,$o" when feat,
+      0 --> 0 by s"$i" when feat
+      ) get i pub o  name "->")
+  }
+
+  def sdrain(i:String,i2:String) = {
+    val feat = v(s"${i}_${i2}")
+    mkConn(newifta ++ (
+      0 --> 0 by s"$i,$i2" when feat,
+      0 --> 0 by s"$i" when feat,
+      0 --> 0 by s"$i2" when feat
+      ) get i get i2  name ">-<")
+  }
+
+  def asdrain(i:String,i2:String) = {
+    val feat = v(s"${i}_${i2}")
+    mkConn(newifta ++ (
+      0 --> 0 by s"$i" when feat,
+      0 --> 0 by s"$i2" when feat
+      ) get i get i2 name ">|<")
+  }
 
   def router(i:String,o1:String,o2:String, outs:String*): IFTA = {
     val feat = v(s"${i}_${o1}_$o2"+(if (outs.isEmpty) "" else outs.mkString("_","_","")))
@@ -29,98 +95,39 @@ object FamConnectors {
     var ifta = newifta ++ (0 --> 0 by i when feat)
     for ( o <- nouts )
       ifta ++= (0 --> 0 by Set(i,o) when feat)
-    ifta get i pub nouts.mkString(",") name "Xor"
+    mkConn(ifta get i pub nouts.mkString(",") name "Xor")
   }
-
-  def fifo(i:String,o:String) = {
-    val feat = v(s"${i}_${o}")
-    newifta ++ (
-      0 --> 1 by s"$i" when feat,
-      1 --> 0 by s"$o" when feat,
-      0 --> 0 by s"$i" when feat
-      ) get i pub o   name "[]"
-  }
-
-  def fifofull(i:String,o:String) = {
-    val feat = v(s"${i}_${o}")
-    newifta ++ (
-      0 --> 1 by s"$o" when feat,
-      1 --> 0 by s"$i" when feat
-      ) get i pub o  name "[.]"
-  }
-
-  def sync(i:String,o:String) = {
-    val feat = v(s"${i}_${o}")
-    newifta ++ (
-      0 --> 0 by s"$i,$o" when feat,
-      0 --> 0 by s"$i" when feat,
-      ) get i pub o  name "->"
-  }
-
-  def sdrain(i:String,i2:String) = {
-    val feat = v(s"${i}_${i2}")
-    newifta ++ (
-      0 --> 0 by s"$i,$i2" when feat,
-      0 --> 0 by s"$i" when feat,
-      0 --> 0 by s"$i2" when feat
-      ) get i get i2  name ">-<"
-  }
-
-  def asdrain(i:String,i2:String) = newifta ++ (
-    0 --> 0 by s"$i" when v(i),
-    0 --> 0 by s"$i2" when v(i2)
-    ) get i get i2 name ">|<"
-
-  //  def join(i:String,i2:String,o:String) = newifta ++ (
-  //    0 --> 0 by s"$i,$i2,$o" when v(i) && v(i2) && v(o),     // default: all ports
-  //    0 --> 0 by s"$i2,$o" when not(v(i)) && v(i2) && v(o),   // can behave as sync if only one input and the output are present
-  //    0 --> 0 by s"$i2" when not(v(i)) && v(i2) && not(v(o)), // can behave as asyncdrain if only one input is present
-  //    0 --> 0 by s"$i,$i2" when v(i) && v(i2) && not(v(o)),   // can behave as syncdrain if only the inputs are present
-  //    0 --> 0 by s"$i,$o" when v(i) && not(v(i2)) && v(o),    // can behave as sync if only one input and the output is present
-  //    0 --> 0 by s"$i" when v(i) && not(v(i2)) && not(v(o)) // can behave as asyncdrain if only one input is present
-  //    ) get i get i2 pub o  when v(o) --> (v(i) || v(i2))
 
   def join(i:String,i2:String,inOrO:String,rest:String*) = {
+    val feat = v(s"${i}_${i2}_$inOrO"+(if (rest.isEmpty) "" else rest.mkString("_","_","")))
     val o = if (rest.isEmpty) inOrO else rest.last
     var ifta = newifta
     val ins = if (rest.nonEmpty) Set(i,i2,inOrO) ++ (rest.toSet - o) else Set(i,i2)
     val ss = (ins + o).subsets().toSet - Set() - Set(o)
     for (s <- ss)
-      ifta ++= (0 --> 0 by s when mkFAnd(mkFeat(s)) && not(mkFOr(mkFeat((ins + o) -- s))))
-    ifta get ins.mkString(",") pub o when v(o) --> mkFOr(mkFeat(ins)) name "+"
+      ifta ++= (0 --> 0 by s when feat)
+    mkConn(ifta get ins.mkString(",") pub o  name "+")
   }
 
-  //  def merger(i:String, i2:String, o:String) = newifta ++ (
-  //    0 --> 0 by s"$i,$o" when v(i) && v(o),    // default
-  //    0 --> 0 by s"$i" when v(i) && not(v(o)),  // can behave as asyncdrain if there is no output
-  //    0 --> 0 by s"$i2,$o" when v(i2) && v(o),  // default
-  //    0 --> 0 by s"$i2" when v(i2) && not(v(o)) // can behave as asyncdrain if there is no output
-  //    ) get i get i2 pub o  when v(o) --> (v(i) || v(i2))
-
   def merger(i:String, i2:String, inOrO:String, rest:String*):IFTA ={
+    val feat = v(s"${i}_${i2}_$inOrO"+(if (rest.isEmpty) "" else rest.mkString("_","_","")))
     val o = if (rest.isEmpty) inOrO else rest.last
     var ifta = newifta
     val ins = if (rest.nonEmpty) Set(i,i2,inOrO) ++ (rest.toSet - o) else Set(i,i2)
     for (i <- ins)
       ifta ++= (
-        0 --> 0 by Set(i,o) when v(i) && v(o),
-        0 --> 0 by Set(i) when v(i) && not(v(o))
+        0 --> 0 by Set(i,o) when feat,
+        0 --> 0 by Set(i) when feat
         )
-    ifta get ins.mkString(",") pub o when v(o) --> mkFOr(mkFeat(ins)) name ">-"
+    mkConn(ifta get ins.mkString(",") pub o when v(o) --> mkFOr(mkFeat(ins)) name ">-")
   }
 
-  //  def repl(i:String,o1:String,o2:String) = newifta ++ (
-  //    0 --> 0 by s"$i,$o1,$o2" when v(i) && v(o1) && v(o2), //default: all ports
-  //    0 --> 0 by s"$i,$o1" when v(i) && v(o1) && not(v(o2)),              // can behave as sync if only one output is present
-  //    0 --> 0 by s"$i,$o2" when v(i) && not(v(o1)) && v(o2),              // can behave as sync if only one output is present
-  //    0 --> 0 by s"$i" when v(i) && not(v(o1) || v(o2))     // can behave as asyncdrain if only the input is present
-  //    ) get i pub o1 pub o2  when (v(o1) || v(o2)) --> v(i)
-
   def repl(i:String,o1:String,o2:String,outs:String*):IFTA = {
+    val feat = v(s"${i}_${o1}_$o2"+(if (outs.isEmpty) "" else outs.mkString("_","_","")))
     val nouts = Set(o1,o2) ++ outs.toSet
     var ifta = newifta
     for (ss <- nouts.subsets())
-      ifta ++= (0 --> 0 by Set(i)++ss when v(i) && mkFAnd(mkFeat(ss)) && not(mkFOr(mkFeat(nouts -- ss)))) // all possible set of outputs + input
-    ifta get i pub nouts.mkString(",") when (mkFOr(mkFeat(nouts))) --> v(i) name "-<"
+      ifta ++= 0 --> 0 by Set(i)++ss when feat
+    mkConn(ifta get i pub nouts.mkString(",") name "-<")
   }
 }
