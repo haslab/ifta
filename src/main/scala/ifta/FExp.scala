9d088f1ac175df324790925c7a9a06968616a6ec
Guillermina Cledou <guillecledou@gmail.com>
2018-06-28 15:09:10 +0100

    Products allowed by a feature expression

--- src/main/scala/ifta/FExp.scala
+++ src/main/scala/ifta/FExp.scala
@@ -1,5 +1,7 @@
 package ifta
 
+import ifta.analyse.{Simplify, Solver}
+
 /**
   * Created by jose on 30/09/16.
   */
@@ -13,6 +15,12 @@ sealed trait FExp {
     case FImp(e1, e2) => e1.feats ++ e2.feats
     case FEq(e1, e2)  => e1.feats ++ e2.feats
   }
+
+  /**
+    * Checks if a given instantiation of features satisfies the feature expression
+    * @param sol instantiation of features
+    * @return
+    */
   def check(sol:Map[String,Boolean]): Boolean = this match {
     case FTrue        => true
     case Feat(name)   => sol.getOrElse(name,false) // elements not in the solution are considered false
@@ -22,6 +30,23 @@ sealed trait FExp {
     // removing syntactic sugar
     case FImp(e1, e2) => (FNot(e1)||e2).check(sol)
     case FEq(e1, e2)  => ((e1-->e2)&&(e2-->e1)).check(sol)
+  }
+
+  /**
+    * Calculates the set of products allowed by the feature expression
+    * w.r.t a set of features
+    * @param fts set of features
+    * @return the set of all valid feature selections, i.e., a set of valid products
+    */
+  def products(fts:Set[String]):Set[Set[String]] = {
+    val fExp = Simplify(this)
+    val ftsNotUsed = fts -- fExp.feats.toSet
+    val fm =
+      if (ftsNotUsed.isEmpty) fExp
+      else  FAnd(fExp,(ftsNotUsed.foldLeft[FExp](FNot(FTrue))(_ || Feat(_)) || Feat("__feat__")))
+    val sols = Solver.all(Simplify(fm))
+    val allSols = (for (so <- sols) yield so.map(s => if (s._2) s._1 else "").toSet.filterNot(_ =="")).toSet
+    allSols.map(s => s.filterNot( _ =="__feat__"))
   }
 
   def &&(other:FExp) = FAnd(this,other)
