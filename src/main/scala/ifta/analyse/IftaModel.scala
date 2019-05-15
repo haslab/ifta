2b1f7fd4a0ac22c2061bb3965ce03470048f1133
Guillermina Cledou <guillecledou@gmail.com>
2019-05-15 14:38:03 +0100

    Experimenting with various models for mcrl2 ifta.  Please enter the commit message for your changes. Lines starting

--- src/main/scala/ifta/analyse/IftaModel.scala
+++ src/main/scala/ifta/analyse/IftaModel.scala
@@ -16,18 +16,77 @@ case class IftaModel(procs: List[Process], init: ProcessExpr)
   extends Model(procs,init) {
 
   override def toString: String = {
-    val actions = procs.flatMap(p => p.getActions).mkString(",")
+    val actions = procs.flatMap(p => p.getActions).filterNot(a => a.name.startsWith("fs")).mkString(",")
     val procsNames = procs.map(p => p.getName.name)
-    val processes = procs.map(p => if (p.isInstanceOf[Channel]) p.toString else renameInitSync(p.asInstanceOf[Init]))
+
     val wrapProcesses = procs.filter(_.isInstanceOf[Channel]).map(c=>mkWrapProc(c.asInstanceOf[Channel]))
-    val fsActions = wrapProcesses.flatMap(_.getActions).map(_.toString.dropRight(4))
+
 
     val nifta = mkNifta(procs)
     val fm = Simplify(nifta.fm && mkFmSyncs(nifta,procs))
     val feats = nifta.iFTAs.flatMap(i => i.feats)
 
-    val inits = renameInitExpr(init)
+//    val inits = renameInitExpr(init)
+//    val initsFsAct = getInitFsActNames(init,procs)
 
+//    var initFsMap = getInitFsMap(procs)
+
+    var processes = procs.map(p => if (p.isInstanceOf[Channel]) p.toString else renameInitSync(p.asInstanceOf[IftaInit]))
+
+    var initsOrder:List[IftaInit] = processes.filter(p => p.isInstanceOf[IftaInit]).map(p => p.asInstanceOf[IftaInit]).sortBy(_.number)
+    var fsMap: Map[ProcessName,(Action,Action,Action)] = Map()
+    println("about to create newIftasInits")
+    var newIftaInits = initsOrder.map(i => i match {
+      case p@IftaInit(num,sa,fsa,List(c1,c2),hide) if c1.name.startsWith("Wrap") && c2.name.startsWith("Wrap")=>
+        println(c1   + " ww   "+ c2)
+        val fs1 = Action("fs"+c1.name.drop(4),Nothing,None)
+        val fs2 = Action("fs"+c2.name.drop(4),Nothing,None)
+        println(fs1 + "   " + fs2)
+        fsMap+= p.getName -> (fs1,fs2,Action(fs1+"_"+fs2,Nothing,None))
+        IftaInit(num,sa,(fs1,fs2),List(c1,c2),hide)
+      case p@IftaInit(num,sa,fsa,List(c1,c2),hide) if c1.name.startsWith("Wrap") && c2.name.startsWith("Init") =>
+        println(c1   + " wi   "+ c2)
+        val fs1 = Action("fs"+c1.name.drop(4),Nothing,None)
+        val fs2 = fsMap(c2)._3
+        println(fs1 + "   " + fs2)
+        fsMap+= p.getName -> (fs1,fs2,Action(fs1+"_"+fs2,Nothing,None))
+        IftaInit(num,sa,(fs1,fs2),List(c1,c2),hide)
+      case p@IftaInit(num,sa,fsa,List(c1,c2),hide) if c1.name.startsWith("Init") && c2.name.startsWith("Wrap") =>
+        println(c1   + " iw   "+ c2)
+        val fs1 = fsMap(c1)._3
+        val fs2 = Action("fs"+c2.name.drop(4),Nothing,None)
+        fsMap+= p.getName -> (fs1,fs2,Action(fs1+"_"+fs2,Nothing,None))
+        println(fs1 + "   " + fs2)
+        IftaInit(num,sa,(fs1,fs2),List(c1,c2),hide)
+      case p@IftaInit(num,sa,fsa,List(c1,c2),hide) if c1.name.startsWith("Init") && c2.name.startsWith("Init") =>
+        println(c1   + " ii   "+ c2)
+        val fs1 = fsMap(c1)._3
+        val fs2 = fsMap(c2)._3
+        println(fs1 + "   " + fs2)
+        fsMap+= p.getName -> (fs1,fs2,Action(fs1+"_"+fs2,Nothing,None))
+        IftaInit(num,sa,(fs1,fs2),List(c1,c2),hide)
+      case p@IftaInit(num,sa,fsa,List(c1),hide) if c1.name.startsWith("Init")  =>
+        println(c1   + "  i  ")
+        val fs1 = Action("",Nothing,None)
+        val fs2 = Action("",Nothing,None)
+        println(fs1 + "   " + fs2)
+        fsMap+= p.getName -> (fs1,fs2,fsMap(c1)._3)
+        IftaInit(num,sa,(fs1,fs2),List(c1),hide)
+      case p@IftaInit(num,sa,fsa,List(c1),hide) if c1.name.startsWith("Wrap") =>
+        println(c1   + "  w  ")
+        val fs1 = Action("",Nothing,None)
+        val fs2 = Action("",Nothing,None)
+        println(fs1 + "   " + fs2)
+        fsMap+= p.getName -> (fs1,fs2,Action("fs"+c1.name,Nothing,None))
+        IftaInit(num,sa,(fs1,fs2),List(c1),hide)
+    })
+
+    println("created newIftasInits"+fsMap)
+
+    processes = processes.filterNot(p => p.isInstanceOf[IftaInit]) ++ newIftaInits
+    val fsActions = wrapProcesses.flatMap(_.getActions).map(_.toString.dropRight(4)) ++ newIftaInits.flatMap(p => p.getActions).filter(a => a.name.startsWith("fs"))
+    val inits = renameInitExpr(init)
+    val initsFsAct = getInitFsActNames(init,procs.filterNot(p => p.isInstanceOf[IftaInit]) ++ newIftaInits)
     val res = s"""
        |sort
        |  Feature = struct ${feats.map(f => s"$f(v:Bool)").mkString(" | ")};
@@ -44,13 +103,46 @@ case class IftaModel(procs: List[Process], init: ProcessExpr)
        |${wrapProcesses.map(_.toStringNoRecursion).mkString("",";\n",";")}
        |
        |init
-       |  block({prod,${fsActions.mkString(",")}},
-       |    comm({prod | ${fsActions.mkString(" | ")} -> product},
+       |  block({prod,${initsFsAct.mkString(",")}},
+       |    comm({prod | ${initsFsAct.mkString(" | ")} -> product},
        |      FM || $inits));
      """.stripMargin
 //  FM || ${if (init.isInstanceOf[ProcessName] && init.asInstanceOf[ProcessName].name.startsWith("Init")) init else "Wrap"+init }));
     res
   }
+
+
+//  private def remakeInits(inits:List[IftaInit]):List[IftaInit] = {
+//    var fsMap: Map[ProcessName,Action] = Map()
+//    inits.map(i =>
+//      IftaInit(i.number,i.syncAct,)
+//    )
+//  }
+
+
+
+  private def getInitFsMap(procs:List[Process]):Map[ProcessName,Action] = {
+    procs.map(p => (p.getName -> getFsName(p,procs))).toMap
+  }
+
+
+  private def getFsName(proc:Process,procs:List[Process]):Action = proc match {
+    case IftaInit(n,sa,fsa,pr,h) => Action(pr.map( p => getFsName(procs.find(a => a.getName == p ).get,procs).name).mkString("_"),Nothing,None)
+    case c@Channel(name,_,_,_,_,_) => Action("fs"+c.getName.toString,Nothing,None)
+  }
+
+  private def getInitFsActNames(init:ProcessExpr,procs:List[Process]): Set[String] = {
+    val procNames = init.getProcNames
+    val initProcNames = procs.filter(p => procNames.contains(p.getName))
+    var res:Set[String] = Set()
+    res = initProcNames.flatMap(i => i match {
+      case p@IftaInit(n,sa,fsa,pr,h) => Set(fsa._1+"_"+fsa._2)
+      case c@Channel(_,_,_,_,_,_) => Set("fs"+c.getName.name.drop(4))
+      case _ => Set[String]()
+    }).toSet
+    res
+  }
+
 
   private def renameInitExpr(expr: ProcessExpr):ProcessExpr = expr match {
     case p@ProcessName(n,ap) if n.startsWith("Init") => p
@@ -58,21 +150,38 @@ case class IftaModel(procs: List[Process], init: ProcessExpr)
     case Par(p1,p2) => Par(renameInitExpr(p1),renameInitExpr(p2))
     case Seq(p1,p2) => Seq(renameInitExpr(p1),renameInitExpr(p2))
     case Block(acts,in) => Block(acts,renameInitExpr(in))
-    case Comm(s,r,in) => Comm(s,r,renameInitExpr(in))
+    case Comm(s,in) => Comm(s,renameInitExpr(in))
     case Hide(acts,in) => Hide(acts,renameInitExpr(in))
     case Sum(vars,p) => Sum(vars,renameInitExpr(p))
     case ITE(c,t,None) => ITE(c,renameInitExpr(t))
     case ITE(c,t,Some(e)) => ITE(c,renameInitExpr(t),Some(renameInitExpr(e)))
   }
 
-  private def renameInitSync(init:Init):Init = {
-    Init(init.number,init.action1,init.action2,
+  private def renameInitSync(init:IftaInit):IftaInit = {
+    IftaInit(init.number,init.syncAct,init.fsSyncAct,
       init.procs.map(p =>
         if (p.name.startsWith("Init"))
           p
         else ProcessName("Wrap"+p.name,p.actualParam)),
       init.toHide)
   }
+
+//  private def remakeInitProcs(inits:List[Init]):List[Init] = {
+//    var init2fsAction:Map[Init,Action] = Map()
+//    for (init <- inits) {
+//       var res = remakeInitProc(init,init2fsAction)
+//        init2fsAction = res._2
+//    }
+//
+//    List()
+//  }
+//
+//  private def remakeInitProc(init:Init,init2fs:Map[Init,Action]):(IftaInit,Map[ProcessName,Action]) = init match {
+//    case Init(num,a1,a2,procs,hide) =>
+//      var fsActs = procs.map(pn => if (pn.name.startsWith("Wrap")) "fs"+pn.name.drop(4) else  )
+//      var iftaInit = IftaInit(num,(a1,a2),  ,procs,hide)
+//
+//  }
 
   private def mkWrapProc(proc:Channel):Channel = {
     val act = Action("fs"+proc.getName,Nothing,None,List(("fs","Product")))
@@ -105,16 +214,16 @@ case class IftaModel(procs: List[Process], init: ProcessExpr)
 
   private def mkFmSyncs(nifta: NIFTA, processes: List[Process]):FExp = {
     val syncs = procs.map(p => p match {
-      case i@Init(_, _, _, _, _) => mkFmSync(nifta,i)
+      case i@IftaInit(_, _, _, _, _) => mkFmSync(nifta,i)
       case _ => FTrue
     })
 
     syncs.foldRight[FExp](FTrue)(_&&_)
   }
 
-  private def mkFmSync(nifta:NIFTA,init:Init):FExp = {
-    val a1 = init.action1.toString
-    val a2 = init.action2.toString
+  private def mkFmSync(nifta:NIFTA,init:IftaInit):FExp = {
+    val a1 = init.syncAct._1.toString
+    val a2 = init.syncAct._2.toString
     val ifta1 = nifta.iFTAs.find(i => i.act.contains(a1))
     val ifta2 = nifta.iFTAs.find(i => i.act.contains(a2))
     (ifta1,ifta2) match {
@@ -146,6 +255,38 @@ case class IftaModel(procs: List[Process], init: ProcessExpr)
      """.stripMargin
   }
 
+}
+
+
+case class IftaInit(number: Option[Int], syncAct:(Action,Action), fsSyncAct:(Action,Action), procs: List[ProcessName], var toHide: Boolean)
+  extends Process{
+  def getOperation: ProcessExpr = {
+    val sync_action = syncAct._1 join syncAct._2
+    val basicProc = procs.tail.foldRight(procs.head : ProcessExpr)((base, p) => Par(base, p))
+    val operator =
+      if (fsSyncAct._1.name!="")
+        Block(List(syncAct._1, syncAct._2,fsSyncAct._1,fsSyncAct._2),
+        Comm(List(
+          (List(syncAct._1, syncAct._2), sync_action),
+          (List(fsSyncAct._1,fsSyncAct._2),fsSyncAct._1 join fsSyncAct._2)),
+          basicProc))
+      else
+        Block(List(syncAct._1, syncAct._2),
+          Comm(List(
+            (List(syncAct._1, syncAct._2), sync_action)),
+            basicProc))
+
+    if(toHide) Hide(List(sync_action), operator)
+    else operator
+  }
+
+  override def toString: String = s"Init${if(number.isDefined) number.get else ""} = ${getOperation.toString}"
+
+  def getActions: Set[Action] = Set(syncAct._1 join syncAct._2, fsSyncAct._1 join fsSyncAct._2 )
+
+  def getName: ProcessName= ProcessName(s"Init${if(number.isDefined) number.get else ""}")
+
+  def toNumberedInit(n: Int): IftaInit = IftaInit(Some(n), syncAct, fsSyncAct, procs, toHide)
 }
 
 
@@ -281,6 +422,35 @@ object IftaModel {
 
       case CPrim(n,_,_,_) => throw new RuntimeException(s"Unknown connector ${n}")
     }
+
+    def buildInits(names1: List[ProcessName], actions1: List[Action], names2: List[ProcessName], actions2: List[Action],initCount:Int):
+    (Map[ProcessName, Process], Int) = {
+      var map: Map[ProcessName, Process] = Map()
+      var fsMap:Map[ProcessName,Action] = Map()
+      var newCount = initCount
+
+      actions1.zip(actions2).zip(names1.zip(names2)).foreach { case ((a1, a2), (n1, n2)) =>
+        val real_name1 = getRealName(map, n1)
+        val real_name2 = getRealName(map, n2)
+        val fsa1:Action = if (real_name1.name.startsWith("Init")) {
+          fsMap.getOrElse(real_name1,Action("fs"+real_name1.name,Nothing,None))
+        }else Action("fs"+real_name1.name,Nothing,None)
+        val fsa2:Action = if (real_name2.name.startsWith("Init")) {
+          fsMap.getOrElse(real_name2,Action("fs"+real_name2.name,Nothing,None))
+        } else Action("fs"+real_name2.name,Nothing,None)
+        val init = IftaInit(Some(newCount), (a1, a2), (fsa1,fsa2),if(real_name1 != real_name2) List(real_name1, real_name2) else List(real_name1), false)
+
+        map += (real_name1 -> init)
+        map += (real_name2 -> init)
+        fsMap += (init.getName -> Action(fsa1.name+"_"+fsa2.name,Nothing,None))
+        newCount += 1
+      }
+      (map,newCount)
+    }
+
+    private def getRealName(map: Map[ProcessName, Process],name: ProcessName): ProcessName =
+      if(map.contains(name)) getRealName(map, map(name).getName)
+      else name
   }
 }
 
